90ea9c87310490a54fc67c2d6ab46de482ecdc11
Kiran <145902144+KiranGadhavi@users.noreply.github.com>
2023-12-12 17:51:56 +0000

    blog project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
 
-  return (
-    <>
-     <h1 className='bg-slate-800 text-white'>blog appwrite</h1>
-    </>
-  )
+ useEffect(()=> {
+  authService.getCurrentUser()
+  .then((userData) => {
+    if(userData){
+      dispatch(login({userData}))
+    }else{
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+ },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+    <Header/>
+    <main>
+    TODO : <Outlet />
+    </main>
+    <Footer/>
+    </div>
+    </div>
+  ) : null
 }
 
 export default App
